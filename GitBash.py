f3ba337088b9911c7245e3875633606d99bf0d9d
Kroum Tzanev <kpym.tzanev@gmail.com>
2017-12-17 10:49:41 +0100

    make it compatible with ST3

--- GitBash.py
+++ GitBash.py
@@ -5,20 +5,21 @@ class GitBashHereCommand(sublime_plugin.WindowCommand):
     settings = sublime.load_settings('GitBash Preferences.sublime-settings')
     gitbash_path = settings.get('gitbash_path')
     if not gitbash_path:
-      print "The \"gitbash_path\" not set in GitBash Preferences.sublime-settings"
+      print("The \"gitbash_path\" not set in GitBash Preferences.sublime-settings")
       return
     gitbash_parameters = settings.get('gitbash_parameters',[])
     if not isinstance(gitbash_parameters, list):
-      print "The \"gitbash_parameters\" must be an array of strings"
+      print("The \"gitbash_parameters\" must be an array of strings")
       return
     view = self.window.active_view()
-    folder_name, file_name = os.path.split(view.file_name())
+    folder_name = os.path.dirname(view.file_name())
     if not folder_name:
-      print 'Can\'t run GitBash : missing folder.'
+      print('Can\'t run GitBash : missing folder.')
       return
-    print u"Starting "+gitbash_path+" "+(" ".join(gitbash_parameters))+" in folder "+folder_name+" ..."
+    print(u"Starting "+gitbash_path+" "+(" ".join(gitbash_parameters))+" in folder "+folder_name+" ...")
     # folder_name is unicode => system encoding ("mbcs" for windows)
-    folder_name = folder_name.encode(sys.getfilesystemencoding())
+    if (not (type(folder_name) is str)):
+      folder_name = folder_name.encode(sys.getfilesystemencoding())
     subprocess.Popen(gitbash_path+" "+(" ".join(gitbash_parameters)), cwd=folder_name)
     # The following works only with gitbash_path="sh" :
     # self.window.run_command("exec", {"cmd": ["start", gitbash_path]+gitbash_parameters, "shell": True, "working_dir": folder_name})
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # SublimeGitBash
 
-GitBash plugin for Sublime Text that adds only one command "git_bash_here".
+GitBash plugin for Sublime Text 2 and 3 that adds only one command "git_bash_here".
 This command runs Git Bash in the current file's folder.
